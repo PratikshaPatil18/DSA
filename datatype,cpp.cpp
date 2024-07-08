442dc89c372cab5d54f6713486092ea4ba41b8e8
Pratiksha Patil <113929757+PratikshaPatil18@users.noreply.github.com>
2024-07-08 18:21:48 +0530

    Data Type in C++
    commit here

---
+++ datatype,cpp.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	cout<<"size of int "<<sizeof(a)<<endl;
+	float b;
+	cout<<"size of float "<<sizeof(b)<<endl;
+	char c;
+	cout<<"size of char "<<sizeof(c)<<endl;
+	bool d;
+	cout<<"size od boolean "<<sizeof(d)<<endl;
+	long e;
+	cout<<"size of long "<<sizeof(e)<<endl;
+	short f;
+	cout<<"size of short "<<sizeof(f)<<endl;
+	
+}
